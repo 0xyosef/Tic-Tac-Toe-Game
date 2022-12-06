1cb8a7c56a9754c808b8372453cf158f95d55c08
YosefJoo <yosef2020go@gmail.com>
2022-12-06 12:30:07 +0200

    project base

---
+++ main.c
@@ -0,0 +1,53 @@
+#include <GL/glut.h>
+
+void drawLine(){
+    glBegin(GL_LINES);//draw line
+    glColor3f(0, 0, 0);
+    glVertex2f(100,50);
+    glVertex2f(100,340);
+
+    glVertex2f(200,340);
+    glVertex2f(200,50);
+
+    glVertex2f(0,150);
+    glVertex2f(300,150);
+
+    glVertex2f(0,250);
+    glVertex2f(300,250);
+    glEnd();
+    //to show draw
+    glFlush();
+
+}
+void display(){
+    //set clear
+    glClearColor(1, 1, 1, 0);
+    //clear buffer color
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawLine();
+}
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc,argv);
+    glutInitWindowPosition(550,200);
+    glutInitWindowSize(300,350);
+    glutCreateWindow("Tic Tac Toe");
+    glutDisplayFunc(display);
+    gluOrtho2D(0,300,0,350);
+    glutMainLoop();
+
+
+//    glutInit(&argc, argv);
+//    //position of the window
+//    glutInitWindowPosition(550,200);
+//    //size of the window
+//    glutInitWindowSize(300,350);
+//    //cereate the window
+//    glutCreateWindow("Tic Tac Toe");
+//    //call the function drawPoint
+//    glutDisplayFunc(display);
+//    //initialize the window
+//    glutMainLoop();
+    return 0;
+}
